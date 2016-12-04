a47d7b7e895542f3049d6aa3fa1a9c94352e1b19
Higher Star <goldbyol@outlook.com>
2016-12-04 09:26:45 +0100

    update module

--- front/modules/partials/gallery.fullscreen.js
+++ front/modules/partials/gallery.fullscreen.js
@@ -1,21 +1,17 @@
 angular
     .module('tlvflats.property')
     .controller('DialogController', DialogController);
-
 DialogController.$inject = ['$scope', '$mdDialog', 'current'];
 function DialogController($scope, $mdDialog, current) {
     var self = this;
     $scope.foo = 'bar';
     $scope.image = current;
-
     $scope.hide = function() {
         $mdDialog.hide();
     };
-
     $scope.cancel = function() {
         $mdDialog.cancel();
     };
-
     $scope.answer = function(answer) {
         $mdDialog.hide(answer);
     };
